d9694aaf214600d3effc1c62c6131040c996781e
mosessatriyo23 <143705697+mosessatriyo23@users.noreply.github.com>
2025-10-26 01:01:59 +0700

    lanjut besok lagi karna ngantuk

---
+++ function.js
@@ -0,0 +1,57 @@
+// LEVEL 1
+
+// function tambah(a, b) {
+//     return a+b;
+// }
+
+// console.log(tambah(5, 3));
+// console.log(tambah(10, 20)); 
+
+// LEVEL 2
+// Ubah function di atas jadi arrow function
+// const tambah = (a, b) => {
+//     a + b;
+// }
+
+// console.log(tambah(2, 3));
+// console.log(tambah(50, 20)); 
+
+// LEVEL 3
+// Buat function yang cek apakah angka ganjil atau genap
+// Return "ganjil" atau "genap"
+// function cekAngka(angka) {
+//     if ( angka %2 === 0) {
+//         return 'genap';
+//     } else {
+//         return 'ganjil';
+//     }
+// }
+
+// console.log(cekAngka(5)); // Output: "ganjil"
+// console.log(cekAngka(10)); // Output: "genap"
+
+// LEVEL 4
+// Buat function yang hitung luas persegi panjang
+// function hitungLuas(panjang, lebar) {
+//     // Tulis kode di sini
+//     return panjang * lebar;
+// }
+
+// console.log(hitungLuas(5, 3)); // Output: 15
+// console.log(hitungLuas(10, 2)); // Output: 20
+
+// LEVEL2
+
+// No1
+// Loop array ini dan console.log setiap buah
+const buah = ['apel', 'jeruk', 'mangga', 'pisang'];
+
+// Cara 1: for loop
+for (let i = 0; i < buah.length; i++) {
+    // Tulis kode di sini
+}
+
+// Cara 2: forEach
+buah.forEach((item) => {
+    // Tulis kode di sini
+});
